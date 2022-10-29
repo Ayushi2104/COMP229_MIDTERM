9d1f8ad322a0a8cbd1876bdec82173759545e8cf
Ayushi2104 <patel.2004aayushi@gmail.com>
2022-10-29 12:06:09 -0400

    First Commit

---
+++ server/config/db.js
@@ -0,0 +1,13 @@
+//module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  //change the URI for this one
+  //"URI":"mongodb+srv://Krunal:jN5RzMHQG23vI5xZ@clustert.ehriwip.mongodb.net/book229?retryWrites=true&w=majority"
+//};
+module.exports =
+ {
+ //"URI":"mongodb://127.0.0.1:27017/book_store"
+ "URI": "mongodb+srv://Ayushi:TgEBqD6zn6jWPZQb@clustert.ph9ittl.mongodb.net/book229?retryWrites=true&w=majority",
+ "Secret":"SomeSecret"
+}
+//TgEBqD6zn6jWPZQb
